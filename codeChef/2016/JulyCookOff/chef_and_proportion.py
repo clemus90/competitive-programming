18066757ade79a967dbf6b0f5a5de0e22be54057
Christian Lemus <crlg90@gmail.com>
2016-07-24 15:22:20 -0500

    solves one exercise from Code Chef's July Cook Off

---
+++ codeChef/2016/JulyCookOff/chef_and_proportion.py
@@ -0,0 +1,17 @@
+def permutate(arr):
+   if len(arr) == 1:
+      yield arr
+   for x in xrange(len(arr)):
+      for perm in permutate(arr[:x] + arr[x+1:]):
+         yield [arr[x]] + perm
+
+vals = [int(x) for x in raw_input().split()]
+
+founded = False
+for val in permutate(vals):
+   if (val[0] / float(val[1]) == val[2] / float(val[3])):
+      print "Possible"
+      founded = True 
+      break
+if not founded:
+   print "Impossible"
