d7570e2edebb0286c9c3c38506a78de5831f173a
Christian Lemus <crlg90@gmail.com>
2016-11-06 16:30:48 -0500

    Solves (not sure about that) stone division using grundy numbers

--- hackerRank/weekOfCode25/stone_division.py
+++ hackerRank/weekOfCode25/stone_division.py
@@ -1,8 +1,22 @@
-n, m = list(map(int, input().split))
+#Review documentation for: Grundy numbers, impartial games, nim game
+#https://www.topcoder.com/community/data-science/data-science-tutorials/algorithm-games/
+#https://www.hackerrank.com/topics/game-theory-and-grundy-numbers
 
-cuts = list(map(int, input().split))
+def build_grundy_numbers(n, divisors, mem_grundy):
+    if n in mem_grundy:
+        return mem_grundy[n]
+    set_g = set()
+    for x in divisors:
+        if n % x == 0:
+            set_g.add(build_grundy_numbers(n // x, divisors, mem_grundy))
+    g = 0
+    while(g in set_g):
+        g+= 1
+    mem_grundy[n] = g
+    return g
 
-def existsWinningGame():
-  pass
+n, m = list(map(int, input().split()))
+cuts = list(map(int, input().split()))
 
-def alwaysLose():
+mem = {}
+print("First" if build_grundy_numbers(n, cuts, mem) != 0 else "Second")
