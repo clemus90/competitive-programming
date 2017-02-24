3c486d6f3de78ddf68fbe3f344c5a8cd27af914f
Christian Lemus <crlg90@gmail.com>
2017-02-24 14:21:20 -0500

    have two approaches for the candidate advance

--- hackerRank/weekOfCode29/MegaprimeNumbers.java
+++ hackerRank/weekOfCode29/MegaprimeNumbers.java
@@ -48,7 +48,7 @@ public class MegaprimeNumbers {
         first = Long.parseLong(inp[0]);
         last = Long.parseLong(inp[1]);
 
-        int countPrimes = 0;
+        long countPrimes = 0;
         MegaPrimeCandidate mpc = new MegaPrimeCandidate(first, last);
         while(mpc.hasNext()){
             long candidate = mpc.next();
@@ -65,11 +65,47 @@ public class MegaprimeNumbers {
                         break;
                     }
                 }
-                if(prime) countPrimes++;
+                if(prime){
+                    countPrimes++;
+                }
+            }
+        }
+        long altCount = 0;
+        for(long candidate=first; candidate<=last; candidate++){
+            long temp = candidate;
+            boolean validCandidate = true;
+            while(temp!=0){
+                int digit = (int)(temp % 10);
+                int[] digits = {2,3,5,7};
+                boolean validDigit = false;
+                for(int i=0; i<digits.length; i++){
+                    if(digit == digits[i]){
+                        validDigit = true;
+                        break;
+                    }
+                }
+                if(!validDigit){
+                    validCandidate = false;
+                    break;
+                }
+                temp /= 10;
+            }
+            if(!validCandidate) continue;
+
+            boolean prime = true;
+            for(int i=0; primes.get(i) * primes.get(i) <= candidate; i++){
+                if(candidate % primes.get(i) == 0){
+                    prime = false;
+                    break;
+                }
+            }
+            if(prime){
+                altCount++;
             }
         }
 
         out.println(countPrimes);
+        out.println(altCount);
 
         out.close();
     }
@@ -97,22 +133,24 @@ class MegaPrimeCandidate implements Iterator<Long> {
     }
 
     public long transformToCandidate(long num){
-        long ret = 0;
-        long base = 1;
-        long digit;
-        boolean correct;
+        int[] digits = new int[(int)(Math.log10(num) + 2)];
+        int index = 0;
+        int digit;
+        boolean correct, transformed;
         boolean carry = false;
         if(num == 0) num++;
         while(num != 0){
-            digit = num % 10;
+            digit = (int)(num % 10);
             if(carry) digit++;
             correct = false;
+            transformed = false;
             for(int i=0; i<oneDigitPrimes.length; i++){
                 if(oneDigitPrimes[i] == digit){
                     correct = true;
                     break;
                 }else if(oneDigitPrimes[i] > digit){
                     correct = true;
+                    transformed = true;
                     digit = oneDigitPrimes[i];
                     break;
                 }
@@ -123,12 +161,24 @@ class MegaPrimeCandidate implements Iterator<Long> {
             }else{
                 carry = false;
             }
-            ret += digit * base;
-            base *= 10;
+            digits[index] = digit;
+            if(transformed){
+                for(int i=index - 1 ; i>=0; i--){
+                    digits[i] = 2;
+                }
+            }
+            index++;
             num /= 10;
         }
         if(carry){
-            ret += oneDigitPrimes[0] * base;
+            digits[index] = oneDigitPrimes[0];
+        }
+
+        long ret = 0;
+        long base = 1;
+        for(int i=0; i<digits.length; i++){
+            ret += digits[i] * base;
+            base *= 10;
         }
         return ret;
     }
