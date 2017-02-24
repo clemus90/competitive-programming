c6a72499b2f2056a8cc8d1fc12eeada10b650a2f
Christian Lemus <crlg90@gmail.com>
2017-02-24 00:44:19 -0500

    creates the iterator for the candidate generation

--- hackerRank/weekOfCode29/MegaprimeNumbers.java
+++ hackerRank/weekOfCode29/MegaprimeNumbers.java
@@ -3,70 +3,133 @@ import java.util.*;
 
 public class MegaprimeNumbers {
 
-    public static ArrayList<Integer> primes = new ArrayList<>(1100000);
-    public static int[] oneDigitPrimes = {2,3,5,7};
+    public static ArrayList<Long> primes = new ArrayList<>(1100000);
     public static int upperBound = 31622777;
+    public static boolean [] nonPrimes = new boolean[upperBound + 1];;
+
 
     public static void genPrimes(){
-        for(int i=0; i<oneDigitPrimes.length; i++){
-            primes.add(oneDigitPrimes[i]);
-        }
-
-        for(int i=9; i<upperBound; i+=2){
-            boolean isPrime = true;
-            int j = 1;
-            int testPrime = primes.get(j);
-            while(testPrime * testPrime <= i){
-                if(i % testPrime == 0){
-                    isPrime = false;
-                    break;
-                }
-                testPrime = primes.get(++j);
-            }
-            if(isPrime){
-                primes.add(i);
-            }
-        }
-    }
-
-    public static void genPrimesSieve(){
-        boolean [] nonPrimes = new boolean[upperBound + 1];
         int prime = 2;
         while(prime * prime <= upperBound){
-            crossPrimes(nonPrimes, prime);
-            prime = getNextPrime(nonPrimes, prime);
+            crossPrimes(prime);
+            prime = getNextPrime(prime);
         }
-        for(int i=0; i<=upperBound; i++){
+        for(int i=2; i<=upperBound; i++){
             if(!nonPrimes[i]){
-                primes.add(i);
+                primes.add((long)i);
             }
         }
     }
 
-    public static void crossPrimes(boolean[] nonPrimes, int prime){
+    public static void crossPrimes(int prime){
         for(int i= prime*prime; i< nonPrimes.length; i+= prime){
             nonPrimes[i] = true;
         }
     }
 
-    public static int getNextPrime(boolean[] nonPrimes, int prime){
+    public static int getNextPrime(int prime){
         int next = prime + 1;
         while(next < nonPrimes.length && nonPrimes[next]){
             next++;
         }
         return next;
     }
+
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out = new PrintWriter(System.out);
 
-        long init = System.currentTimeMillis();
-        //genPrimes();
-        genPrimesSieve();
-        init = System.currentTimeMillis() - init;
-        out.println("time " + init);
-        out.println(primes.size());
+        genPrimes();
+
+        long first, last;
+
+        String[] inp = br.readLine().split(" ");
+
+        first = Long.parseLong(inp[0]);
+        last = Long.parseLong(inp[1]);
+
+        int countPrimes = 0;
+        MegaPrimeCandidate mpc = new MegaPrimeCandidate(first, last);
+        while(mpc.hasNext()){
+            long candidate = mpc.next();
+            if(candidate <= upperBound){
+                if(!nonPrimes[(int)candidate]){
+                    countPrimes++;
+                }
+            }
+            else{
+                boolean prime = true;
+                for(int i=0; primes.get(i) * primes.get(i) <= candidate; i++){
+                    if(candidate % primes.get(i) == 0){
+                        prime = false;
+                        break;
+                    }
+                }
+                if(prime) countPrimes++;
+            }
+        }
+
+        out.println(countPrimes);
 
         out.close();
+    }
+}
+
+class MegaPrimeCandidate implements Iterator<Long> {
+
+    private int[] oneDigitPrimes = {2,3,5,7};
+    private long currVal;
+    private long upperBound;
+
+    public MegaPrimeCandidate(long num, long upperBound){
+        currVal = transformToCandidate(num);
+        this.upperBound = upperBound;
+    }
+
+    public boolean hasNext(){
+        return currVal <= upperBound;
+    }
+
+    public Long next(){
+        long curr = currVal;
+        currVal = transformToCandidate(curr + 1);
+        return curr;
+    }
+
+    public long transformToCandidate(long num){
+        long ret = 0;
+        long base = 1;
+        long digit;
+        boolean correct;
+        boolean carry = false;
+        if(num == 0) num++;
+        while(num != 0){
+            digit = num % 10;
+            if(carry) digit++;
+            correct = false;
+            for(int i=0; i<oneDigitPrimes.length; i++){
+                if(oneDigitPrimes[i] == digit){
+                    correct = true;
+                    break;
+                }else if(oneDigitPrimes[i] > digit){
+                    correct = true;
+                    digit = oneDigitPrimes[i];
+                    break;
+                }
+            }
+            if(!correct){
+                digit = oneDigitPrimes[0];
+                carry = true;
+            }else{
+                carry = false;
+            }
+            ret += digit * base;
+            base *= 10;
+            num /= 10;
+        }
+        if(carry){
+            ret += oneDigitPrimes[0] * base;
+        }
+        return ret;
     }
 }
